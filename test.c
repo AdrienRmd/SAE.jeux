a5c290b8c0892dac4387dffacd0a43d423478b68
floriansenicourt <floriansenicourt@gmail.com>
2026-01-12 10:16:06 +0100

    feat(base) : added folders for different languages and the main function to take a value from file

--- test.c
+++ test.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <string.h>
 
-const char *filename = "input.txt";
+const char *filename = "fr/intro-FR.txt";
 
-int main(int argc, char *argv[])
+char value_ligne(int target_line, char *file_contents, FILE *in_file) {
+    int current_line = 1;
+
+    while (fscanf(in_file, "%[^\n] ", file_contents) != EOF) {
+        if (current_line == target_line) {
+            return *file_contents;
+        }
+        current_line++;
+    }
+    return '\0';
+}
+
+int main()
 {
     // open the file
     FILE *in_file = fopen(filename, "r");
@@ -15,6 +28,8 @@ int main(int argc, char *argv[])
 
     // allocation of the character array
     char *file_contents = malloc(sb.st_size);
+
+    printf("%c\n", value_ligne(3, file_contents, in_file));
 
     // display line by line
     while (fscanf(in_file, "%[^\n] ", file_contents) != EOF)
