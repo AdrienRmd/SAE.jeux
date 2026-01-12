a8ad59acf718ccce25c8d3f204d3db6b750cad03
remond.adrien <adrien.remond@protonmail.com>
2026-01-12 17:09:38 +0100

    feat(biome): restructure and expand biome generation system
    Reorganize biomes into difficulty categories (easy, medium, hard, special) and add multiple new biome types including flower fields, volcanic prairies, dark forests, demoniac prairies, caves, villages, ruins, and casinos. Enhanced volcanic prairie biome with additional properties for temperature, lava activity, eruption risk, mineral richness, and toxic fumes. Updated enumeration and added comprehensive biome structures for more diverse gameplay environments.

--- function/generation_biome.c
+++ function/generation_biome.c
@@ -1,6 +1,6 @@
 // structures des biomes
 
-// plaines
+// biome facile
 typedef struct
 {
     int difficulty_biome;
@@ -14,12 +14,94 @@ typedef struct
 
 } desert;
 
-// foret
+// champ_de_fleur
 typedef struct
 {
     int difficulty_biome;
 
-} foret;
+} champ_de_fleur;
+
+// prairie
+typedef struct
+{
+    int difficulty_biome;
+
+} prairie;
+
+// banquise
+typedef struct
+{
+    int difficulty_biome;
+
+} banquise;
+
+// biomes moyens - lac
+typedef struct
+{
+    int difficulty_biome;
+
+} lac;
+
+// biomes moyens - marais
+typedef struct
+{
+    int difficulty_biome;
+
+} marais;
+
+// biomes moyens - canyon
+typedef struct
+{
+    int difficulty_biome;
+
+} canyon;
+
+// biomes moyens - jungle
+typedef struct
+{
+    int difficulty_biome;
+
+} jungle;
+
+// biomes moyens - mer
+typedef struct
+{
+    int difficulty_biome;
+
+} mer;
+
+// biomes durs - prairie_volcanique
+typedef struct
+{
+    int difficulty_biome;
+    int temperature;      // Temperature of the volcanic area
+    int lava_level;       // Level of lava activity (1-10)
+    int eruption_risk;    // Risk of eruption (percentage)
+    int mineral_richness; // Richness of volcanic minerals
+    int toxic_fumes;      // Level of toxic gases in the air
+
+} prairie_volcanique;
+
+// biomes durs - foret_noire
+typedef struct
+{
+    int difficulty_biome;
+
+} foret_noire;
+
+// biomes durs - prairie_demoniaque
+typedef struct
+{
+    int difficulty_biome;
+
+} prairie_demoniaque;
+
+// biomes durs - marais_sombres
+typedef struct
+{
+    int difficulty_biome;
+
+} marais_sombres;
 
 // montagne
 typedef struct
@@ -28,6 +110,41 @@ typedef struct
 
 } montagne;
 
+// biomes marches spéciaux - volcan
+typedef struct
+{
+    int difficulty_biome;
+
+} volcan;
+
+// foret
+typedef struct
+{
+    int difficulty_biome;
+
+} foret;
+
+// biomes marches spéciaux - grotte
+typedef struct
+{
+    int difficulty_biome;
+
+} grotte;
+
+// biomes marches spéciaux - village
+typedef struct
+{
+    int difficulty_biome;
+
+} village;
+
+// biomes marches spéciaux - mine
+typedef struct
+{
+    int difficulty_biome;
+
+} mine;
+
 // coffre
 typedef struct
 {
@@ -35,35 +152,70 @@ typedef struct
 
 } salle_coffre;
 
+// lieux spéciaux - marché
+typedef struct
+{
+    int difficulty_biome;
+
+} marché;
+
+// lieux spéciaux - ruines
+typedef struct
+{
+    int difficulty_biome;
+
+} ruines;
+
+// lieux spéciaux - casino
+typedef struct
+{
+    int difficulty_biome;
+
+} casino;
+
+// lieux spéciaux - salle_coffre_legend
+typedef struct
+{
+    int difficulty_biome;
+
+} salle_coffre_legend;
+
 // enum des terrains possible
 typedef enum
 {
+    // biomes faciles
     plaine = 1,
     desert = 2,
     champ_de_fleur = 3,
     prairie = 4,
     banquise = 5,
 
+    // biomes moyens
     lac = 6,
     marais = 7,
     canyon = 8,
     jungle = 9,
     mer = 10,
 
-    volcan = 11,
-    foret = 12,
-    casino = 13,
-    marché = 14,
-    mine = 15,
+    // biomes durs
+    prairie_volcanique = 11,
+    foret_noire = 12,
+    prairie_demoniaque = 13,
+    marais_sombres = 14,
+    montagne = 15,
 
-    prairie_volcanique = 16,
-    foret_noire = 17,
-    prairie_demoniaque = 18,
-    marais_sombres = 19,
-    montagne = 20,
+    // biomes marches spéciaux
+    volcan = 16,
+    foret = 17,
+    grotte = 18,
+    village = 19,
+    mine = 20,
 
+    // lieux spéciaux
     salle_coffre = 21,
-    ruines = 22,
-    grotte = 23,
+    marché = 22,
+    ruines = 23,
+    casino = 24,
+    salle_coffre_legend = 25,
 
 } Terrain;
--- function/generation_biome.h
+++ function/generation_biome.h
@@ -1,4 +1,203 @@
 #ifndef GEN_BIOME_H
 #define GEN_BIOME_H
 
+// biome facile
+
+// plaine
+typedef struct
+{
+    int difficulty_biome;
+} plaine;
+
+// desert
+typedef struct
+{
+    int difficulty_biome;
+} desert;
+
+// champ_de_fleur
+typedef struct
+{
+    int difficulty_biome;
+} champ_de_fleur;
+
+// prairie
+typedef struct
+{
+    int difficulty_biome;
+} prairie;
+
+// banquise
+typedef struct
+{
+    int difficulty_biome;
+} banquise;
+
+// biomes moyens
+
+// lac
+typedef struct
+{
+    int difficulty_biome;
+} lac;
+
+// marais
+typedef struct
+{
+    int difficulty_biome;
+} marais;
+
+// canyon
+typedef struct
+{
+    int difficulty_biome;
+} canyon;
+
+// jungle
+typedef struct
+{
+    int difficulty_biome;
+} jungle;
+
+// mer
+typedef struct
+{
+    int difficulty_biome;
+} mer;
+
+// biomes durs
+
+// prairie_volcanique
+typedef struct
+{
+    int difficulty_biome;
+} prairie_volcanique;
+
+// foret_noire
+typedef struct
+{
+    int difficulty_biome;
+} foret_noire;
+
+// prairie_demoniaque
+typedef struct
+{
+    int difficulty_biome;
+} prairie_demoniaque;
+
+// marais_sombres
+typedef struct
+{
+    int difficulty_biome;
+} marais_sombres;
+
+// montagne
+typedef struct
+{
+    int difficulty_biome;
+} montagne;
+
+// biomes marches spéciaux
+
+// volcan
+typedef struct
+{
+    int difficulty_biome;
+} volcan;
+
+// foret
+typedef struct
+{
+    int difficulty_biome;
+} foret;
+
+// grotte
+typedef struct
+{
+    int difficulty_biome;
+} grotte;
+
+// village
+typedef struct
+{
+    int difficulty_biome;
+} village;
+
+// mine
+typedef struct
+{
+    int difficulty_biome;
+} mine;
+
+// lieux spéciaux
+
+// salle_coffre
+typedef struct
+{
+    int difficulty_biome;
+} salle_coffre;
+
+// marché
+typedef struct
+{
+    int difficulty_biome;
+} marché;
+
+// ruines
+typedef struct
+{
+    int difficulty_biome;
+} ruines;
+
+// casino
+typedef struct
+{
+    int difficulty_biome;
+} casino;
+
+// salle_coffre_legend
+typedef struct
+{
+    int difficulty_biome;
+} salle_coffre_legend;
+
+// enum des terrains possible
+typedef enum
+{
+    // biomes faciles
+    plaine = 1,
+    desert = 2,
+    champ_de_fleur = 3,
+    prairie = 4,
+    banquise = 5,
+
+    // biomes moyens
+    lac = 6,
+    marais = 7,
+    canyon = 8,
+    jungle = 9,
+    mer = 10,
+
+    // biomes durs
+    prairie_volcanique = 11,
+    foret_noire = 12,
+    prairie_demoniaque = 13,
+    marais_sombres = 14,
+    montagne = 15,
+
+    // biomes marches spéciaux
+    volcan = 16,
+    foret = 17,
+    grotte = 18,
+    village = 19,
+    mine = 20,
+
+    // lieux spéciaux
+    salle_coffre = 21,
+    marché = 22,
+    ruines = 23,
+    casino = 24,
+    salle_coffre_legend = 25,
+} Terrain;
+
 #endif
