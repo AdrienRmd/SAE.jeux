e3517a8a39dccbab078e564dcedcc1351c552992
floriansenicourt <floriansenicourt@gmail.com>
2026-01-12 15:46:17 +0100

    feat(file management): added file for other functions and added the include that is necessary

---
+++ function/other.h
@@ -0,0 +1,6 @@
+#ifndef GESTION_FICHIERS_H
+#define GESTION_FICHIERS_H
+
+int tirage_nombre(int minimum, int maximum);
+
+#endif
--- main.c
+++ main.c
@@ -6,44 +6,8 @@
 #include "function/gamef.h"
 
 
-/**
- * @brief Generates a random integer between minimum and maximum (inclusive)
- *
- * This function returns a random integer between the specified minimum and maximum values.
- * The range is inclusive, meaning both minimum and maximum values can be returned.
- *
- * @param minimum The minimum value to generate (inclusive)
- * @param maximum The maximum value to generate (inclusive)
- * @return A random integer between minimum and maximum
- *
- * @example
- * int nombre = tirage_nombre(1, 100); // Returns a random number between 1 and 100
- * int de = tirage_nombre(1, 6);     // Returns a random number between 1 and 6
- *
- * @note This function initializes the random seed automatically on first call
- * @warning Ensure maximum >= minimum to avoid undefined behavior
- */
-int tirage_nombre(int minimum, int maximum)
-{ // automatiquement la graine aléatoire (srand(time(NULL))).
-    static int initialized = 0;
-    if (!initialized)
-    {
-        srand(time(NULL));
-        initialized = 1;
-    }
-    return minimum + rand() % (maximum - minimum + 1);
-}
-
-
 int main()
 {
-    // Test the tirage_nombre function
-    printf("Testing random number generation:\n");
-    for (int i = 0; i < 10; i++)
-    {
-        printf("Random number between 1 and 100: %d\n", tirage_nombre(1, 100));
-    }
-
     int language = intro();
 
     if (language == 0)
