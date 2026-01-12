74fb4489314a7d325113e2d14d0b0a1cd46e7b05
floriansenicourt <floriansenicourt@gmail.com>
2026-01-12 11:23:07 +0100

    feat(function) : added function value_line

--- test.c
+++ test.c
@@ -3,41 +3,49 @@
 #include <sys/stat.h>
 #include <string.h>
 
-const char *filename = "fr/intro-FR.txt";
+int value_line(const char *filename, int line, char *buffer_destination, int length){
+    FILE *file = fopen(filename, 'r');
+    int i = 1;
+    while (fgets(buffer_destination, length, file) != NULL) {
+        if (i == line) {
+            buffer_destination[strcspn(buffer_destination, "\n")] = 0;
 
-char value_ligne(int target_line, char *file_contents, FILE *in_file) {
-    int current_line = 1;
-
-    while (fscanf(in_file, "%[^\n] ", file_contents) != EOF) {
-        if (current_line == target_line) {
-            return *file_contents;
+            fclose(file);
+            return 1;
         }
-        current_line++;
+        i++;
     }
-    return '\0';
+
+    fclose(file);
+    return 0;
 }
+
+
 
 int main()
 {
-    // open the file
-    FILE *in_file = fopen(filename, "r");
+    /*// open the file
+    FILE *file = fopen(file, "r");
 
     // read the file size
     struct stat sb;
-    stat(filename, &sb);
+    stat(file, &sb);
 
     // allocation of the character array
     char *file_contents = malloc(sb.st_size);
 
-    printf("%c\n", value_ligne(3, file_contents, in_file));
-
     // display line by line
-    while (fscanf(in_file, "%[^\n] ", file_contents) != EOF)
+    while (fscanf(file, "%[^\n] ", file_contents) != EOF)
     {
         printf("> %s\n", file_contents);
     }
 
     // close file
-    fclose(in_file);
-    exit(EXIT_SUCCESS);
+    fclose(file);
+    exit(EXIT_SUCCESS);*/
+
+    char buffer[256];
+
+    value_line("fr/intro-FR.txt", 3, buffer, sizeof(buffer));
+    printf("%s", buffer);
 }
