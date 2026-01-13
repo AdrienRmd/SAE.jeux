af0978840e5da4d8d6638697a850d3c2758241f2
floriansenicourt <floriansenicourt@gmail.com>
2026-01-13 09:48:06 +0100

    feat(function): added ongoing_floor and final floor headers

--- function/gamef.h
+++ function/gamef.h
@@ -8,4 +8,8 @@ void player_init(char *filename);
 
 /*void battle(Player player, Monster monster, int lang);*/
 
+int ongoing_floor(char lang, int biome, int floor);
+
+void final_floor(char lang);
+
 #endif
