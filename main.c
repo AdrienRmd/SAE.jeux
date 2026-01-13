318ce8b9c2a50c2364ba842c6428c0cbb3f14b3d
floriansenicourt <floriansenicourt@gmail.com>
2026-01-13 08:49:10 +0100

    feat(game): added const for the number of floors and the loop for all of these

--- main.c
+++ main.c
@@ -5,19 +5,34 @@
 #include <time.h>
 #include "function/gamef.h"
 
+const NB_ETAGE = 10;
 
 int main()
 {
-    int language = intro();
+    char language = intro();
+
 
     if (language == 'f' || language == 'F')
-    {
+    {   
+        language = 'fr';
         player_init("fr/intro-FR.txt");
 
+        for (int etage = 1 ; etage < NB_ETAGE ; etage++){
+
+        }
+
+        etage_final("%c/final.txt", language);
     }
     else if (language == 'e' || language == 'E')
     {
+        language = 'en';
         player_init("en/intro-EN.txt");
+
+        for (int etage = 1 ; etage < NB_ETAGE ; etage++){
+
+        }
+
+        etage_final("%c/final.txt", language);
     }
 
     return 0;
