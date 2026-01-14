9f534be747c88fc8920a3ece4618f08050ddac82
floriansenicourt <floriansenicourt@gmail.com>
2026-01-14 08:52:24 +0100

    feat(biome): enhance biome selection logic and add language support

--- function/gamef.c
+++ function/gamef.c
@@ -33,13 +33,10 @@ void player_init(char *filename)
 void choose_random_biome(int floor, int *biome){
 
     int random;
+    biome[0] = 0;
 
     for (int i = 0 ; i < 2 ; i++){
-        if (random_number(0, 9) == 0 && biome[0] < 21){
-            random = random_number(21, 25);
-        }
-
-        else if (floor == 1 || floor == 2){
+        if (floor == 1 || floor == 2){
             random = random_number(1, 5);
         }
         else if (floor == 3 || floor == 4){
@@ -53,16 +50,51 @@ void choose_random_biome(int floor, int *biome){
         }
         biome[i] = random;
     }
+    while (biome[0] == biome[1]){
+        if (floor == 1 || floor == 2){
+            random = random_number(1, 5);
+        }
+        else if (floor == 3 || floor == 4){
+            random = random_number(6, 10);
+        }
+        else if (floor == 5 || floor == 6){
+            random = random_number(11, 15);
+        }
+        else {
+            random = random_number(16, 20);
+        }
+        biome[1] = random;
+    }
+    
 }
 
-int choose_biome(int *biome){
+int choose_biome(int *biome, const char *lang){
 
-    int choice;
-    int biome1 = 10 * (biome[0] - 1) + 2;
-    int biome2 = 10 * (biome[1] - 1) + 2;
+    int choice = -1;
+    char filepath[100];
+    char biome1[100], biome2[100];
+    
+    // Clear input buffer
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+    
+    sprintf(filepath, "%s/biomes/biome.txt", lang);
+    value_line(filepath, 10 * (biome[0] - 1) + 2, biome1, sizeof(biome1));
+    value_line(filepath, 10 * (biome[1] - 1) + 2, biome2, sizeof(biome2));
     printf("Quel biome voulez-vous explorer ?\n");
-    printf("1 - %d / 2 - %d : ", biome1, biome2);
-    scanf("%d", &choice);
+    
+    while (choice != 1 && choice != 2) {
+        printf("1 - %s / 2 - %s : ", biome1, biome2);
+        fflush(stdout);
+        int result = scanf("%d", &choice);
+        while (getchar() != '\n');
+        
+        if (result != 1 || (choice != 1 && choice != 2)) {
+            printf("Choix invalide. Veuillez entrer 1 ou 2.\n");
+            choice = -1;
+        }
+    }
+    
     if (choice == 1){
         return biome[0];
     }
