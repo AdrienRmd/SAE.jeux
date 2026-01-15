cab27f5547717edd1ebdacefc64c4ed593b56138
floriansenicourt <floriansenicourt@gmail.com>
2026-01-15 21:29:53 +0100

    feat(monster): implement set_monster function to load monster data from file

--- function/gamef.c
+++ function/gamef.c
@@ -364,6 +364,68 @@ int get_monster_id(int biome_id, const char *lang)
     return atoi(buffer);
 }
 
+void set_monster(Monster *monster, const char *language, int monster_id)
+{
+    char monster_buffer[128];
+    char filename[256];
+    int current_line = 1;
+    int found = 0;
+
+    sprintf(filename, "%s/monstre/monster.txt", language);
+
+    // Search for the monster by checking each block of 9 lines
+    while (!found && current_line < 1000)
+    {
+        // Try to read ID from current position
+        if (value_line(filename, current_line, monster_buffer, sizeof(monster_buffer)))
+        {
+            int found_id = atoi(monster_buffer);
+
+            if (found_id == monster_id)
+            {
+                // Found the correct monster, load its data
+                found = 1;
+                monster->id = monster_id;
+
+                // Load the remaining 8 lines of monster data
+                value_line(filename, current_line + 1, monster_buffer, sizeof(monster_buffer));
+                strcpy(monster->name, monster_buffer);
+
+                value_line(filename, current_line + 2, monster_buffer, sizeof(monster_buffer));
+                monster->hp = atoi(monster_buffer);
+
+                value_line(filename, current_line + 3, monster_buffer, sizeof(monster_buffer));
+                monster->att = atoi(monster_buffer);
+
+                value_line(filename, current_line + 4, monster_buffer, sizeof(monster_buffer));
+                monster->def = atoi(monster_buffer);
+
+                value_line(filename, current_line + 5, monster_buffer, sizeof(monster_buffer));
+                monster->dodge = atoi(monster_buffer);
+
+                value_line(filename, current_line + 6, monster_buffer, sizeof(monster_buffer));
+                monster->spe = atoi(monster_buffer);
+
+                value_line(filename, current_line + 7, monster_buffer, sizeof(monster_buffer));
+                monster->luck = atoi(monster_buffer);
+
+                value_line(filename, current_line + 8, monster_buffer, sizeof(monster_buffer));
+                monster->rarity = atoi(monster_buffer);
+            }
+            else
+            {
+                // Move to next monster (skip 9 lines, but check for empty line)
+                current_line += 10;
+            }
+        }
+        else
+        {
+            // Can't read from file, break
+            break;
+        }
+    }
+}
+
 int ongoing_floor(const char *lang, int biome_id, int floor)
 {
     Monster monster;
